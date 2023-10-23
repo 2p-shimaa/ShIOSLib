71af589648f06b2ebe9db1ffa7d864b72d5f8a84
shimaa khairy <shkhairy@2p.com.sa>
2023-10-23 20:30:53 +0300

    add logger file and update pod url at podspec file

---
+++ Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  ShIOSLib
+//
+//  Created by shimaa khairy on 23/10/2023.
+//
+
+import Foundation
+public class Logger{
+    public func printHello(){
+        print("Hello Shimaa")
+    }
+}
